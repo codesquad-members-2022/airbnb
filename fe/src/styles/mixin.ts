b816a5ac93bc585627b0a819623de350d883782b
soralee <sora2821@gmail.com>
2022-05-25 15:10:21 +0900

    design: defaultButtonTransition 추가

--- fe/src/styles/mixin.ts
+++ fe/src/styles/mixin.ts
@@ -5,4 +5,16 @@ export const mixin = {
     justify-content: ${jc};
     align-items: ${ai};
   `,
+
+  defaultButtonTransition: () => `
+    transition: opacity 300ms;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &:active {
+      opacity: 0.7;
+    }
+  `,
 };

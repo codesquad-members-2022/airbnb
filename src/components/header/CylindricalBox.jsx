d4ebaca2c81a08204f8e523de003c56ea4acb6d9
nanjeong <whdwkxj7@naver.com>
2022-05-25 21:36:08 +0900

    refactor: 아이콘 svg 태그 assets 폴더로 분리

--- src/components/header/CylindricalBox.jsx
+++ src/components/header/CylindricalBox.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
+import React from "react";
 import styled from "styled-components";
 import Cylindrical from "../../cylindrical";
+import {ReactComponent as CrossIcon} from "../../assets/crossIcon.svg";
+import {ReactComponent as SearchIcon} from "../../assets/searchIcon.svg";
 
 // eslint-disable-next-line react/prop-types
 const CylindricalBox = ({title, placeHolder, style, partId, clickedPart, setClickedPart, hasSearchButton}) => {
@@ -25,24 +27,18 @@ const CylindricalBox = ({title, placeHolder, style, partId, clickedPart, setClic
             </TextBox>
             {isClicked && (
                 <Icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                    <CrossIcon />
                 </Icon>
             )}
             {hasSearchButton &&
                 (isClicked ? (
                     <CylindricalSearchButton style={cylindricalSearchButtonStyle}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
+                        <SearchIcon />
                         <span>검색</span>
                     </CylindricalSearchButton>
                 ) : (
                     <CircleSearchButton>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
+                        <SearchIcon />
                     </CircleSearchButton>
                 ))}
         </CylindricalButton>
--- src/components/header/LoginButton.jsx
+++ src/components/header/LoginButton.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Cylindrical from "../../cylindrical";
+import {ReactComponent as MenuIcon} from "../../assets/menuIcon.svg";
+import {ReactComponent as UserIcon} from "../../assets/userIcon.svg";
 
 const LoginButton = () => {
     const cylindricalStyle = {
@@ -12,16 +14,12 @@ const LoginButton = () => {
 
     return (
         <LoginButtonBox style={cylindricalStyle}>
-            <Icon>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </Icon>
-            <UserIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                </svg>
-            </UserIcon>
+            <IconBox>
+                <MenuIcon />
+            </IconBox>
+            <UserIconBox>
+                <UserIcon />
+            </UserIconBox>
         </LoginButtonBox>
     );
 };
@@ -37,12 +35,12 @@ const LoginButtonBox = styled(Cylindrical)`
     }
 `;
 
-const Icon = styled.div`
+const IconBox = styled.div`
     color: ${({theme}) => theme.color.gray2};
     width: 18px;
 `;
 
-const UserIcon = styled(Icon)`
+const UserIconBox = styled(IconBox)`
     background-color: ${({theme}) => theme.color.gray3};
     border-radius: 50%;
     color: ${({theme}) => theme.color.white};

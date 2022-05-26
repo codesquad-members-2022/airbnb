970719dbb6adf5ec62ef0309ef70f18f6484d944
who-hoo <who.ho3ov@gmail.com>
2022-05-26 14:13:30 +0900

    🚑️ 누락된 MemberResponse DTO 작성
    Co-Authored-By: WooJin Park(Ader) <29879110+ak2j38@users.noreply.github.com>

---
+++ BE/src/main/java/com/ahoo/airbnb/member/MemberResponse.java
@@ -0,0 +1,15 @@
+package com.ahoo.airbnb.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponse {
+
+	private long id;
+	private String name;
+	private String profileImageUrl;
+}

7b35303c4ca3a92f21b492360b651a64a7a607c9
WooJin Park(Ader) <ak2j38@gmail.com>
2022-05-26 15:37:22 +0900

    ✨ feature : 숙소 상세정보 API 구현
    1. 숙소 상세정보 API를 위한 Image, Images(일급컬렉션) 추가
    2. RoomDetailResponse DTO 파일 추가
    
    end

---
+++ BE/src/main/java/com/ahoo/airbnb/room/Image.java
@@ -0,0 +1,13 @@
+package com.ahoo.airbnb.room;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+    private int sequence;
+    private String imageUrl;
+}
---
+++ BE/src/main/java/com/ahoo/airbnb/room/Images.java
@@ -0,0 +1,14 @@
+package com.ahoo.airbnb.room;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Images {
+    private List<Image> images;
+
+    public Images(List<Image> images) {
+        this.images = images;
+    }
+}

8213bffe4b141c25476e7dace28557682c72a942
BumgeunSong <isp1195@gmail.com>
2022-05-27 14:32:48 +0900

    Add: Month Creator for month section header

--- iOS/Targets/Cherrybnb/Sources/Search/Model/DayCreator.swift
+++ iOS/Targets/Cherrybnb/Sources/Search/Model/DayCreator.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 
-enum DayCreationError: Error {
+enum CalendarError: Error {
     case metadataGeneration
+    case offsetMonthGeneration
 }
 
 struct DayCreator {
@@ -19,9 +20,9 @@ struct DayCreator {
     
     // 기준일로부터 offset만큼 떨어진 달의 Day를 return.
     func daysInMonth(for basedate: Date, monthOffset: Int) -> [Day] {
-        guard let basedate = calendar.date(byAdding: .month, value: monthOffset, to: basedate),
-              let firstDayOfOffsetMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: basedate)) else {
-            print(DayCreationError.metadataGeneration)
+        guard let firstDayOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: basedate)),
+              let firstDayOfOffsetMonth = calendar.date(byAdding: .month, value: monthOffset, to: firstDayOfMonth) else {
+            print(CalendarError.offsetMonthGeneration)
             return []
         }
         
@@ -31,7 +32,7 @@ struct DayCreator {
     // 기준일이 속한 달의 Day를 return.
     func daysInMonth(for basedate: Date) -> [Day] {
         guard let metadata = try? monthMetadata(for: basedate) else {
-            print(DayCreationError.metadataGeneration)
+            print(CalendarError.metadataGeneration)
             return []
         }
         
@@ -47,7 +48,7 @@ struct DayCreator {
     private func monthMetadata(for basedate: Date) throws -> MonthMetaData  {
         guard let numberOfDaysInMonth = calendar.range(of: .day, in: .month, for: basedate)?.count,
               let firstDayOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: basedate)) else {
-            throw DayCreationError.metadataGeneration
+            throw CalendarError.metadataGeneration
         }
         
         let weekDayOfFirstDay = calendar.component(.weekday, from: firstDayOfMonth)
@@ -56,3 +57,15 @@ struct DayCreator {
     }
 }
 
+struct MonthCreator {
+    let calendar = Calendar(identifier: .gregorian)
+    
+    func createMonth(for basedate: Date, monthOffset: Int) -> Month? {
+        guard let firstDayOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: basedate)),
+              let firstDayOfOffsetMonth = calendar.date(byAdding: .month, value: monthOffset, to: firstDayOfMonth) else {
+            print(CalendarError.offsetMonthGeneration)
+            return nil
+        }
+        return Month(date: firstDayOfOffsetMonth)
+    }
+}

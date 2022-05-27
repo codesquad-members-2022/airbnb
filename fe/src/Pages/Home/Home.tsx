e0f0f6bb94c6eb9c5f6f638445492e780d95b9ef
youryu0212 <youryu0212@naver.com>
2022-05-27 21:45:25 +0900

    rename: Calendar 컴포넌트 폴더명 변경 (CalendarModal -> Calendar)

--- fe/src/Pages/Home/Home.tsx
+++ fe/src/Pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import CalendarModal from "Components/CalendarModal/Calendar";
+import CalendarModal from "Components/Calendar/Calendar";
 import Gnb from "Components/Gnb/Gnb";
 import SearchBar from "Components/SearchBar/SearchBar";
 import { getTodayDate } from "Helpers/utils";

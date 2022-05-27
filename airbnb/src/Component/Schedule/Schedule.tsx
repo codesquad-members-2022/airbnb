ba3aa6337bdcf5cebd0902a4ed15d8aa207a20ec
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-05-27 13:40:40 +0900

    refactor: 달력 일짜 계산 로직 수정

--- airbnb/src/Component/Schedule/Schedule.tsx
+++ airbnb/src/Component/Schedule/Schedule.tsx
@@ -3,36 +3,28 @@ import * as S from './ScheduleStyle';
 
 const WEEKDAY: string[] = ['일', '월', '화', '수', '목', '금', '토'];
 
-const getMonthDate = (year: number, month: number): [number[]] => {
-  let fullDate: number = 32 - new Date(year, month, 32).getDate();
-  let firstDate: number = new Date(year, month, 1).getDay();
-  const isFeburary: number = fullDate % 7;
-  const DateArr: object[] = new Array(isFeburary ? 5 : 4).fill([]);
-  let flag = 1;
+const getMonthDate = (year: number, month: number) => {
+  const fullDate: number = 32 - new Date(year, month, 32).getDate(); // 해달 달이 총 몇일인지
+  const firstDate: number = -new Date(year, month, 1).getDay(); // 1일이 몇요일인지
+  const addDate = fullDate + firstDate;
 
-  // TODO: 리팩토링
-  const result = DateArr.map(week => {
-    for (let i = 0; i < WEEKDAY.length; i += 1) {
-      if (firstDate > 0 || fullDate <= 0) {
-        firstDate -= 1;
-        week = [...week, 0];
-      } else {
-        week = [...week, flag];
-        flag += 1;
-        fullDate -= 1;
-      }
-    }
-    return week;
-  });
-  return result;
+  const firstWeek = new Array(WEEKDAY.length).map((_, idx) => firstDate + idx);
+
+  const currentMonth = Array.from(
+    { length: Math.floor(addDate / WEEKDAY.length) },
+    (_, i) =>
+      firstWeek.map(v => {
+        const day = v + i * 7;
+        return day < 1 || day > fullDate ? null : day;
+      }),
+  );
+  return currentMonth;
 };
 
 export function Schedule(): JSX.Element {
   const testYear = 2022;
   const tesyMonth = 4;
-
   const testFullDate = getMonthDate(testYear, tesyMonth);
-
   return (
     <S.ScheduleWrapper>
       <S.Calendar>
@@ -41,8 +33,11 @@ export function Schedule(): JSX.Element {
             <li key={v4()}>{value}</li>
           ))}
         </S.WeekDay>
-
-        <S.WeekDate>{testFullDate[]}</S.WeekDate>
+        <S.WeekDate>
+          {/* week.map 돌때 {} 감싸고 ul 태그 더해주기 */}
+          {testFullDate.map(week => week.map(day => <li key={v4()}>{day}</li>))}
+        </S.WeekDate>
+        <S.WeekDate>{}</S.WeekDate>
       </S.Calendar>
       <S.Calendar>d</S.Calendar>
     </S.ScheduleWrapper>

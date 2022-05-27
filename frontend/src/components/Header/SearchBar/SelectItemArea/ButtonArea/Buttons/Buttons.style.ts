8393badad2e1a14549fc4d7a2426035e103e32c1
BB-choi <78826879+BB-choi@users.noreply.github.com>
2022-05-27 17:44:23 +0900

    refactor: 불필요한 코드 삭제

--- frontend/src/components/Header/SearchBar/SelectItemArea/ButtonArea/Buttons/Buttons.style.ts
+++ frontend/src/components/Header/SearchBar/SelectItemArea/ButtonArea/Buttons/Buttons.style.ts
@@ -3,22 +3,12 @@ import { styled } from "@mui/material/styles";
 
 import { RoundButtonProps } from "@types";
 
-type ButtonName = "closeButton" | "searchButton";
-const closeButton: ButtonName = "closeButton";
-const searchButton: ButtonName = "searchButton";
-
-type ColorName = "grey4" | "primary" | "grey1" | "white";
-const grey1: ColorName = "grey1";
-const grey4: ColorName = "grey4";
-const primary: ColorName = "primary";
-const white: ColorName = "white";
-
 const RoundButton = styled(IconButton)<RoundButtonProps>(
   ({ icon, theme: { palette, size } }) => {
     const isCloseIcon = icon === "close";
-    const buttonKeyName = isCloseIcon ? closeButton : searchButton;
-    const bgColor = isCloseIcon ? grey4 : primary;
-    const svgColor = isCloseIcon ? grey1 : white;
+    const buttonKeyName = isCloseIcon ? "closeButton" : "searchButton";
+    const bgColor = isCloseIcon ? "grey4" : "primary";
+    const svgColor = isCloseIcon ? "grey1" : "white";
 
     return `  
     width: ${size.searchBar[buttonKeyName].width};

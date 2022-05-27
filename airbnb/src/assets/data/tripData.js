864b0b26b7371cf4850fb31393557c35126a5adb
seungchanHan <seungchanhan00@gmail.com>
2022-05-27 11:37:23 +0900

    style : [4] cursor : pointer 변경 및 데이터 수정

--- airbnb/src/assets/data/tripData.js
+++ airbnb/src/assets/data/tripData.js
@@ -1,24 +1,24 @@
-import * as I from '../image/trip/app';
+import * as ImageSrc from '../image/trip/app';
 
 export const tripData = [
   {
     id: 1,
     content: '자연생활을 만끽할 수 있는 숙소',
-    img: I.Nature,
+    img: ImageSrc.Nature,
   },
   {
     id: 2,
     content: '독특한 주거 공간',
-    img: I.Water,
+    img: ImageSrc.Water,
   },
   {
     id: 3,
     content: '집 전체',
-    img: I.House,
+    img: ImageSrc.House,
   },
   {
     id: 4,
     content: '반려동물 동반 가능',
-    img: I.Dog,
+    img: ImageSrc.Dog,
   },
 ];

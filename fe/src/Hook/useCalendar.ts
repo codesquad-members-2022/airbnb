0ae73d7fb647b7cc32f1e2b82a37eb8d5c1e170c
youryu0212 <youryu0212@naver.com>
2022-05-27 16:43:05 +0900

    feat: calendar useReducer, 컨텍스트 구현

---
+++ fe/src/Hook/useCalendar.ts
@@ -0,0 +1,73 @@
+import { CalendarContext, DispatchCalendarContext } from "Context/CalendarProvider";
+import { useContext, useReducer } from "react";
+
+interface actionType {
+  type: string;
+  year?: number;
+  month?: number;
+  day?: number;
+}
+
+const initialState = {
+  isOpen: false,
+  checkIn: {
+    year: 0,
+    month: 0,
+    day: 0,
+  },
+  checkOut: {
+    year: 0,
+    month: 0,
+    day: 0,
+  },
+};
+
+const reducer = (state: object, action: actionType) => {
+  switch (action.type) {
+    case "OPEN":
+      return {
+        ...state,
+        isOpen: true,
+      };
+    case "CLOSE":
+      return {
+        ...state,
+        isOpen: false,
+      };
+    case "SET_CHECK_IN":
+      return {
+        ...state,
+        checkIn: {
+          year: action.year,
+          month: action.month,
+          day: action.day,
+        },
+      };
+    case "SET_CHECK_OUT":
+      return {
+        ...state,
+        checkOut: {
+          year: action.year,
+          month: action.month,
+          day: action.day,
+        },
+      };
+    case "RESET":
+      return {
+        ...initialState,
+        isOpen: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export function useCalendarReducer() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return [state, dispatch];
+}
+
+export function useCalendar() {
+  const [state, dispatch] = [useContext(CalendarContext), useContext(DispatchCalendarContext)];
+  return [state, dispatch];
+}

a73f3f39b1ad5ebf9f09aaff7e78c19926df6698
WooJin Park(Ader) <ak2j38@gmail.com>
2022-05-27 11:48:54 +0900

    ✨ 숙소, 위시 Swagger 적용 기능 구현
    1. RoomController Swagger 적용
    2. WishController Swagger 적용
    3. HelloController 삭제
    
    end

--- BE/src/main/java/com/ahoo/airbnb/room/RoomController.java
+++ BE/src/main/java/com/ahoo/airbnb/room/RoomController.java
@@ -3,6 +3,12 @@ package com.ahoo.airbnb.room;
 import com.ahoo.airbnb.room.dtos.RoomDetailResponse;
 import com.ahoo.airbnb.room.dtos.RoomRequest;
 import com.ahoo.airbnb.room.dtos.RoomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "rooms", description = "숙소 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +25,19 @@ public class RoomController {
 
     private final MockRoomService roomService;
 
-    /**
-     * 숙소 리스트
-     *
-     * @param roomRequest
-     * @return
-     */
+    @Operation(summary = "숙소 리스트 조회",
+        description = "모든 숙소를 조회합니다.",
+        responses = {
+            @ApiResponse(responseCode = "200",
+                description = "숙소 리스트 조회 성공",
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        array = @ArraySchema(schema = @Schema(implementation = RoomResponse.class))
+                    )
+                })
+        }
+    )
     @PostMapping("/rooms")
     public ResponseEntity<List<RoomResponse>> getRooms(@RequestBody RoomRequest roomRequest) {
 
@@ -31,12 +45,19 @@ public class RoomController {
         return ResponseEntity.ok().body(roomService.findByCondition(roomRequest));
     }
 
-    /**
-     * 숙소 상세 정보
-     *
-     * @param id
-     * @return
-     */
+    @Operation(summary = "숙소 상세 조회",
+        description = "숙소의 상세 정보 및 이미지를 조회합니다.",
+        responses = {
+            @ApiResponse(responseCode = "200",
+                description = "숙소 상세 조회 성공",
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = RoomDetailResponse.class)
+                    )
+                })
+        }
+    )
     @GetMapping("/rooms/{id}")
     public ResponseEntity<RoomDetailResponse> getRoomDetails(@PathVariable Long id) {
 
--- BE/src/main/java/com/ahoo/airbnb/wish/WishController.java
+++ BE/src/main/java/com/ahoo/airbnb/wish/WishController.java
@@ -1,6 +1,12 @@
 package com.ahoo.airbnb.wish;
 
 import com.ahoo.airbnb.room.dtos.RoomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "wishes", description = "위시 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +28,19 @@ public class WishController {
 
     private final MockWishService wishService;
 
+    @Operation(summary = "위시 리스트 조회",
+        description = "모든 위시를 조회합니다.",
+        responses = {
+            @ApiResponse(responseCode = "200",
+                description = "위시 리스트 조회 성공",
+                content = {
+                    @Content(
+                        mediaType = "application/json",
+                        array = @ArraySchema(schema = @Schema(implementation = RoomResponse.class))
+                    )
+                })
+        }
+    )
     @GetMapping
     public ResponseEntity<List<RoomResponse>> getWishes() {
 
@@ -28,6 +48,14 @@ public class WishController {
         return ResponseEntity.ok().body(wishService.findAll());
     }
 
+    @Operation(summary = "위시 등록",
+        description = "위시리스트에 등록합니다.",
+        responses = {
+            @ApiResponse(responseCode = "204",
+                description = "위시 등록 성공"
+            )
+        }
+    )
     @PostMapping("/{roomId}")
     public ResponseEntity<Void> create(@PathVariable Long roomId, @RequestParam Long wishId) {
 
@@ -36,6 +64,14 @@ public class WishController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "위시 삭제",
+        description = "위시리스트에서 삭제합니다.",
+        responses = {
+            @ApiResponse(responseCode = "204",
+                description = "위시 삭제 성공"
+            )
+        }
+    )
     @DeleteMapping("/{wishId}")
     public ResponseEntity<Void> delete(@PathVariable Long wishId) {
 

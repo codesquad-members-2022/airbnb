423bcff4f4eeb437d7553ba0f6e43ae0c961eb95
seungchanHan <seungchanhan00@gmail.com>
2022-05-27 15:54:20 +0900

    feat : [4] 달력 2개 생성

--- airbnb/src/components/Calendar/CalendarDates.jsx
+++ airbnb/src/components/Calendar/CalendarDates.jsx
@@ -1,4 +1,3 @@
-import { theme } from '@chakra-ui/react';
 import React from 'react';
 import styled from 'styled-components';
 
--- airbnb/src/components/Calendar/CalendarModal.jsx
+++ airbnb/src/components/Calendar/CalendarModal.jsx
@@ -1,3 +1,4 @@
+import { Flex } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CalendarBody from './CalendarBody';
@@ -10,12 +11,14 @@ function Modal() {
 
   const [month, setMonth] = useState(MONTH);
   const [year, setYear] = useState(YEAR);
-  const [totalDate, setTotalDate] = useState([]);
+  const [earlyTotalDate, setEarlyTotalDate] = useState([]);
+  const [lastTotalDate, setLastTotalDate] = useState([]);
 
   const changeDate = (month) => {
     //이전 날짜
     let PVLastDate = new Date(YEAR, month - 1, 0).getDate();
     let PVLastDay = new Date(YEAR, month - 1, 0).getDay();
+
     //다음 날짜
     const ThisLastDay = new Date(YEAR, month, 0).getDay();
     const ThisLastDate = new Date(YEAR, month, 0).getDate();
@@ -27,6 +30,7 @@ function Modal() {
         PREVIOUSDATE_ARR.unshift(PVLastDate - i);
       }
     }
+
     //다음 날짜 만들기
     let NEXTDATE_ARR = [];
     for (let i = 1; i < 7 - ThisLastDay; i++) {
@@ -45,29 +49,57 @@ function Modal() {
   };
 
   useEffect(() => {
-    setTotalDate(changeDate(7));
-  }, []);
+    setEarlyTotalDate(changeDate(month));
+  }, [month]);
 
   useEffect(() => {
-    setTotalDate(changeDate(month));
+    setLastTotalDate(changeDate(month + 1));
   }, [month]);
 
   return (
-    <ModalContainer>
-      <CalendarHead year={year} month={month} setMonth={setMonth} />
-      <CalendarBody totalDate={totalDate} />
-    </ModalContainer>
+    <>
+      <ModalContainer>
+        <Flex justify="space-between">
+          <EarlyMonth>
+            <CalendarHead
+              year={year}
+              month={month}
+              setMonth={setMonth}
+              position={'leftBtn'}
+            />
+            <CalendarBody totalDate={earlyTotalDate} />
+          </EarlyMonth>
+
+          <LateMonth>
+            <CalendarHead
+              year={year}
+              month={month + 1}
+              setMonth={setMonth}
+              position={'rightBtn'}
+            />
+            <CalendarBody totalDate={lastTotalDate} />
+          </LateMonth>
+        </Flex>
+      </ModalContainer>
+    </>
   );
 }
 
 const ModalContainer = styled.div`
   position: absolute;
   background-color: ${({ theme }) => theme.colors.white};
-  border-radius: 20px;
+  border-radius: 30px;
   width: 870px;
-  height: 460px;
+  height: 499px;
   top: 190px;
   z-index: 10;
 `;
 
+const EarlyMonth = styled.div`
+  float: right;
+`;
+
+const LateMonth = styled.div`
+  float: left;
+`;
 export default Modal;

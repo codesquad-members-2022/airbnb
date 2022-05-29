84f41fc1fdfb9bcb600dbbd949ea20d65d26ab17
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-05-29 19:48:06 +0900

    feat: 커스텀 훅과 타입 가드를 통한 컨텍스트 상태 사용 구현

--- airbnb/src/Context/Searching/index.tsx
+++ airbnb/src/Context/Searching/index.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, Dispatch } from 'react';
+import React, { useReducer, Dispatch, useContext } from 'react';
 
 const calendarInitialState: CalenderState = {
   startDate: 0,
@@ -17,16 +17,12 @@ const customersInitialState: CustomerState = {
   smallChildCount: 0,
 };
 
-export const SearchingContext = React.createContext<SearchingContextState>({
-  calendar: calendarInitialState,
-  price: priceInitialState,
-  customers: customersInitialState,
-});
+export const SearchingContext =
+  React.createContext<SearchingContextState | null>(null);
 
 // TODO: Dispatch 함수 해결해야할것
-export const setSerachingContext = React.createContext<SearchContextDispatch>(
-  () => {},
-);
+export const SearchingDispatchContext =
+  React.createContext<SearchContextDispatch | null>(null);
 
 const calenderReducer = (
   state: CalenderState,
@@ -95,14 +91,36 @@ export function SearchingProvider({
 
   return (
     <SearchingContext.Provider value={{ calendar, price, customers }}>
-      <setSerachingContext.Provider
+      <SearchingDispatchContext.Provider
         value={{ calendarDispatch, priceDispatch, customersDispatch }}
       >
         {children}
-      </setSerachingContext.Provider>
+      </SearchingDispatchContext.Provider>
     </SearchingContext.Provider>
   );
 }
+
+export const useSearchBarState = () => {
+  const searchBarState = useContext(SearchingContext);
+
+  // 타입가드 -> null or 초기 상태 값이 들어 올수 있어서 사용하는 곳에서 구조분해할당등을 못했지만
+  // 런타임 타입 체크로 searchBar의 값이 있는 지 없는 지를 검사할 경우 type은 정해져있는 상태임
+  if (!searchBarState) {
+    throw new Error('Seraching Bar State Error');
+  }
+
+  return searchBarState;
+};
+
+export const useSearchBarDispatch = () => {
+  const searchBarDispatch = useContext(SearchingDispatchContext);
+
+  if (!searchBarDispatch) {
+    throw new Error('Searching Bar Dispatch Error');
+  }
+
+  return searchBarDispatch;
+};
 
 export interface SearchingContextState {
   calendar: CalenderState;

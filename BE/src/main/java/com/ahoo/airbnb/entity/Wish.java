64c00b8643e6fc74c1a79d7b9783c9d9f5b52849
WooJin Park(Ader) <ak2j38@gmail.com>
2022-05-31 03:00:46 +0900

    ✨ Wish 도메인 구현
    1. 누락된 Wish 도메인 구현
    
    end
    
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

---
+++ BE/src/main/java/com/ahoo/airbnb/entity/Wish.java
@@ -0,0 +1,28 @@
+package com.ahoo.airbnb.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "wish")
+public class Wish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}

a21251f072057e9b395fb7ac2de62a1eff265f7a
godhyeongman <aaaa4000@naver.com>
2022-05-30 21:13:38 +0900

    rename: Component -> Components, Context -> Contexts 폴더 네이밍 컨벤션 수정

--- airbnb/src/Components/Schedule/Schedule.tsx
+++ airbnb/src/Components/Schedule/Schedule.tsx
@@ -5,15 +5,18 @@ const WEEKDAY: string[] = ['일', '월', '화', '수', '목', '금', '토'];
 
 const getMonthDate = (year: number, month: number) => {
   const fullDate: number = 32 - new Date(year, month, 32).getDate(); // 해달 달이 총 몇일인지
-  const firstDate: number = -new Date(year, month, 1).getDay(); // 1일이 몇요일인지
+  const firstDate: number = new Date(year, month, 1).getDay(); // 1일이 몇요일인지
   const addDate = fullDate + firstDate;
 
-  const firstWeek = new Array(WEEKDAY.length)
-    .fill(0)
-    .map((_, idx) => firstDate + idx);
+  const firstWeek = Array.from(
+    { length: WEEKDAY.length },
+    (_, idx) => idx - firstDate + 1,
+  );
 
   const currentMonth = Array.from(
-    { length: Math.floor(addDate / WEEKDAY.length) },
+    {
+      length: Math.floor(addDate / WEEKDAY.length + 1),
+    },
     (_, i) =>
       firstWeek.map(v => {
         const day = v + i * 7;
@@ -25,18 +28,21 @@ const getMonthDate = (year: number, month: number) => {
 
 export function Schedule(): JSX.Element {
   const testYear = 2022;
-  const tesyMonth = 4;
+  const tesyMonth = 1;
   const testFullDate = getMonthDate(testYear, tesyMonth);
+  const testNextFullDate = getMonthDate(testYear, tesyMonth + 1);
   return (
     <S.ScheduleWrapper>
       <S.Calendar>
+        <S.Date>
+          {testYear}년 {tesyMonth + 1}월
+        </S.Date>
         <S.WeekDay>
           {WEEKDAY.map(value => (
             <li key={v4()}>{value}</li>
           ))}
         </S.WeekDay>
 
-        {/* week.map 돌때 {} 감싸고 ul 태그 더해주기 */}
         {testFullDate.map(week => (
           <S.WeekDate key={v4()}>
             {week.map(day => (
@@ -44,10 +50,22 @@ export function Schedule(): JSX.Element {
             ))}
           </S.WeekDate>
         ))}
-
-        <S.WeekDate>{}</S.WeekDate>
       </S.Calendar>
-      <S.Calendar>d</S.Calendar>
+
+      <S.Calendar>
+        <S.WeekDay>
+          {WEEKDAY.map(value => (
+            <li key={v4()}>{value}</li>
+          ))}
+        </S.WeekDay>
+        {testNextFullDate.map(week => (
+          <S.WeekDate key={v4()}>
+            {week.map(day => (
+              <li key={v4()}>{day}</li>
+            ))}
+          </S.WeekDate>
+        ))}
+      </S.Calendar>
     </S.ScheduleWrapper>
   );
 }

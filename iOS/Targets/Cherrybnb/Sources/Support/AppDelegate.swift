0902ade330ced65059c285be6f702a74388cde0b
BumgeunSong <isp1195@gmail.com>
2022-05-30 17:32:00 +0900

    Chore: Delete unnecessary code in appDelegate

--- iOS/Targets/Cherrybnb/Sources/Support/AppDelegate.swift
+++ iOS/Targets/Cherrybnb/Sources/Support/AppDelegate.swift
@@ -10,10 +10,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-//        let storyboard = UIStoryboard(name: "Main", bundle: .main)
-//        let viewController = storyboard.instantiateInitialViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController()
 
-        let viewController = try? CalendarPickerViewController(basedate: Calendar(identifier:  .gregorian).getNextDay(for: Date(), offset: 0)!, numOfMonths: 12, didDateSelect: nil, didDataRangeSelect: nil)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
 

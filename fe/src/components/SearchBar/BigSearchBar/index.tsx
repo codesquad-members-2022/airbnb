4a7302af3b66e793889f2178013902e35eabddc6
Cola <bcad1591@gmail.com>
2022-06-01 06:34:28 +0900

    chore: TODO 추가

--- fe/src/components/SearchBar/BigSearchBar/index.tsx
+++ fe/src/components/SearchBar/BigSearchBar/index.tsx
@@ -18,13 +18,12 @@ function BigSearchBar() {
         checkOut={{ year: 2022, month: 10, day: 10 }}
       />
       <S.Separator />
-
       <PriceButton minPrice={100} maxPrice={100} />
       <S.Separator />
-
       <GuestsButton child={1} />
 
-      <S.SearchButtonLayer>
+      {/*TODO: 위 세가지 필드중 하나라도 채워져야 검색 버튼 활성화*/}
+      <S.SearchButtonLayer disabled={false}>
         <SearchButton>검색</SearchButton>
       </S.SearchButtonLayer>
     </S.SearchBarLayer>

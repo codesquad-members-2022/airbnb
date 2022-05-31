5c37ae95648ec32fa102d5f8e09ff4ca0dbad29c
WooJin Park(Ader) <ak2j38@gmail.com>
2022-05-31 18:09:16 +0900

    ✨ 날짜 계산을 위한 Util 클래스 구현
    1. 평일 날짜 수, 주말 날짜 수, 두 날짜 사이의 일 수를 반환하는 메소드 추가
    
    end
    
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

---
+++ BE/src/main/java/com/ahoo/airbnb/utils/DateUtils.java
@@ -0,0 +1,63 @@
+package com.ahoo.airbnb.utils;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
+public class DateUtils {
+
+    private DateUtils() {
+
+    }
+
+    /**
+     *
+     * @param from (inclusive)
+     * @param to (exclusive)
+     * @return 평일 날짜 수
+     */
+    public static int getWeekdayCount(LocalDateTime from, LocalDateTime to) {
+        int weekdayCount = 0;
+
+        for (LocalDate date = from.toLocalDate(); date.isBefore(to.toLocalDate()); date = date.plusDays(1)) {
+            if (!isWeekend(date)) {
+                weekdayCount++;
+            }
+        }
+
+        return weekdayCount;
+    }
+
+    /**
+     *
+     * @param from (inclusive)
+     * @param to (exclusive)
+     * @return 주말 날짜 수
+     */
+    public static int getWeekendCount(LocalDateTime from, LocalDateTime to) {
+        int weekendCount = 0;
+
+        for (LocalDate date = from.toLocalDate(); date.isBefore(to.toLocalDate()); date = date.plusDays(1)) {
+            if (isWeekend(date)) {
+                weekendCount++;
+            }
+        }
+
+        return weekendCount;
+    }
+
+    private static boolean isWeekend(LocalDate date) {
+        return date.getDayOfWeek() == DayOfWeek.FRIDAY || date.getDayOfWeek() == DayOfWeek.SATURDAY;
+    }
+
+    /**
+     *
+     * @param from
+     * @param to
+     * @return 두 날짜 사이의 일수
+     */
+    public static int getBetweenDays(LocalDateTime from, LocalDateTime to) {
+        return Period.between(from.toLocalDate(), to.toLocalDate()).getDays();
+    }
+}

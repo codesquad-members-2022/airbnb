b20e97ddd30e63f062fc12d78e69422cb2a59cf4
Damagucci-Juice <jsr9874@gmail.com>
2022-05-31 16:12:31 +0900

    feat: Calendar VC 초기작업

--- iOS/Airbnb/Airbnb/Search/BackgroundViewController.swift
+++ iOS/Airbnb/Airbnb/Search/BackgroundViewController.swift
@@ -17,12 +17,28 @@ class BackgroundViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpNavigationAppearance()
+        setUpToolBarAppearance()
     }
     
-    func setUpNavigationAppearance() {
+    private func setUpNavigationAppearance() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemGray6
         navigationItem.standardAppearance = appearance
         navigationItem.scrollEdgeAppearance = appearance
+    }
+    
+    private func setUpToolBarAppearance() {
+        navigationController?.toolbar.tintColor = .black
+        let toolBarAppearance = UIToolbarAppearance()
+        toolBarAppearance.configureWithOpaqueBackground()
+        toolBarAppearance.backgroundColor = .systemGray6
+        navigationController?.toolbar.isTranslucent = true
+        navigationController?.toolbar.standardAppearance = toolBarAppearance
+        navigationController?.toolbar.compactAppearance = toolBarAppearance
+        if #available(iOS 15.0, *) {
+            navigationController?.toolbar.scrollEdgeAppearance = toolBarAppearance
+        }
     }
 }

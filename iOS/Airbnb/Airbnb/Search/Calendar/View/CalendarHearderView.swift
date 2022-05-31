e45dc2e334ece81d4146ca20721014a0a966e1bf
Damagucci-Juice <jsr9874@gmail.com>
2022-05-31 17:54:40 +0900

    feat: 달력 그리기 조악하게 성공

---
+++ iOS/Airbnb/Airbnb/Search/Calendar/View/CalendarHearderView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarHearderView.swift
+//  Airbnb
+//
+//  Created by YEONGJIN JANG on 2022/05/31.
+//
+
+import UIKit
+
+class CalendarHearderView: UICollectionReusableView {
+//    override var reuseIdentifier: String?
+}
+
+extension UICollectionReusableView {
+//    static var reuseIdentifier: String { String(describing: Self.self) }
+}
+
+
--- iOS/Airbnb/Airbnb/Search/Calendar/View/CalendarViewController.swift
+++ iOS/Airbnb/Airbnb/Search/Calendar/View/CalendarViewController.swift
@@ -11,6 +11,8 @@ class CalendarViewController: BackgroundViewController, CommonViewControllerProt
     
     let reservationModel: ReservationModel
     let calendarModel = SearchCalendarModel()
+    var resultArray: [CalendarResult] = []
+    var date = Date()
     
     private let calendar = Calendar(identifier: .gregorian)
     
@@ -20,9 +22,10 @@ class CalendarViewController: BackgroundViewController, CommonViewControllerProt
         layout.minimumInteritemSpacing = 0
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.isScrollEnabled = false
+        collectionView.isScrollEnabled = true
+        collectionView.isPagingEnabled = true
         //TODO: - 캘린더 완성되면 지울 내용 - 빨간 배경색
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .systemBackground
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
@@ -41,6 +44,16 @@ class CalendarViewController: BackgroundViewController, CommonViewControllerProt
         attribute()
         layout()
         bind()
+        // 12달 만들기
+        for _ in 0..<12 {
+            let nextMonthResult = calendarModel.getNextMonthDays(from: date)
+            self.resultArray.append(nextMonthResult)
+            self.date = nextMonthResult.date
+        }
+        collectionView.register(CalendarViewCell.self, forCellWithReuseIdentifier: CalendarViewCell.reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.reloadData()
     }
     
     func attribute() {
@@ -75,5 +88,33 @@ class CalendarViewController: BackgroundViewController, CommonViewControllerProt
         //TODO: - graphic 관련한 뷰컨트롤러로 변경
         let nextVC = PriceGraphViewController()
         self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
+
+extension CalendarViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        35
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        self.resultArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: CalendarViewCell.reuseIdentifier, for: indexPath) as? CalendarViewCell else {
+            return UICollectionViewCell()
+        }
+        guard self.resultArray[indexPath.section].result.count - 1 >= indexPath.row else {
+            return cell
+        }
+        let day = self.resultArray[indexPath.section].result[indexPath.row]
+        cell.day = day
+        return cell
+    }
+}
+
+extension CalendarViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.collectionView.frame.width / 7, height: self.collectionView.frame.width / 7)
     }
 }

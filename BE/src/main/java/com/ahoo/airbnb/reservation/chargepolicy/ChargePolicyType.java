4471a7fa7ff06764499950937fde0f60210127bf
WooJin Park(Ader) <ak2j38@gmail.com>
2022-06-01 00:53:00 +0900

    ✨️ 요금 정책 열거형 타입에 요금 정책 추가
    1. ChargePolicyType 열거형 타입 값 추가
    
    end
    
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

--- BE/src/main/java/com/ahoo/airbnb/reservation/chargepolicy/ChargePolicyType.java
+++ BE/src/main/java/com/ahoo/airbnb/reservation/chargepolicy/ChargePolicyType.java
@@ -2,7 +2,11 @@ package com.ahoo.airbnb.reservation.chargepolicy;
 
 public enum ChargePolicyType {
     WEEKDAY("주중요금정책", WeekdayChargePolicy.getInstance()),
-    WEEKEND("주말요금정책", WeekendChargePolicy.getInstance());
+    WEEKEND("주말요금정책", WeekendChargePolicy.getInstance()),
+    WEEKLY_DISCOUNT("주단위할인정책", WeeklyDiscountChargePolicy.getInstance()),
+    SERVICE("서비스요금정책", ServiceChargePolicy.getInstance()),
+    CLEANING("청소비요금정책", CleaningChargePolicy.getInstance()),
+    ACCOMMODATION_TAX("주말요금정책", AccommodationTaxChargePolicy.getInstance());
 
     private String policyName;
     private ChargePolicy chargePolicy;
@@ -11,7 +15,6 @@ public enum ChargePolicyType {
         this.policyName = policyName;
         this.chargePolicy = chargePolicy;
     }
-
 
     public String getPolicyName() {
         return policyName;

24576e11dd3b7ce2d699d922ee8a21e74f005392
WooJin Park(Ader) <ak2j38@gmail.com>
2022-06-02 03:52:04 +0900

    ♻️ 롬복을 이용한 생성자 추가 및 연관관계 재설정
    1. @NoArgsConstructor, @AllArgsConstructor 추가
    2. Room과 RoomImage와의 양방향 연관관계 매핑 제거 및 단방향으로 설정
    
    end
    
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

--- BE/src/main/java/com/ahoo/airbnb/entity/RoomImage.java
+++ BE/src/main/java/com/ahoo/airbnb/entity/RoomImage.java
@@ -1,27 +1,24 @@
 package com.ahoo.airbnb.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 @Table(name = "room_image")
 public class RoomImage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "room_id")
-    private Room room;
 
     private int sequence;
     private String url;

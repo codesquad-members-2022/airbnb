dfa01b72baf2888728d277aa51bc2654f3578dc9
Kang Changgu (Miller) <rkckr1@naver.com>
2022-06-03 02:48:06 +0900

    feat: GlobalExceptionHandler 내 BindException 핸들러 추가

--- backend/src/main/java/com/codesquad/airbnb/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/codesquad/airbnb/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,15 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleMethodArgumentNotValidException(
         MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+            .forEach(e -> errors.put(((FieldError) e).getField(), e.getDefaultMessage()));
+        return errors;
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBindException(BindException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors()
             .forEach(e -> errors.put(((FieldError) e).getField(), e.getDefaultMessage()));

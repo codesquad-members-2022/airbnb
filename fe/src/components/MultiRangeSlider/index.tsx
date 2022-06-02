c2fbaf617de92a23e9b69293f8fd0c0d9cd2e9f6
Cola <bcad1591@gmail.com>
2022-06-03 00:31:04 +0900

    refactor: 초기 inputValue를 설정하는 로직 추가

--- fe/src/components/MultiRangeSlider/index.tsx
+++ fe/src/components/MultiRangeSlider/index.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
+
+import { useInputRangeSetter } from '@/components/MultiRangeSlider/context/InputRange';
+
 import { IPriceRange } from './common';
 import LeftInput from './LeftInput';
 import RightInput from './RightInput';
 import * as S from './style';
 
 function MultiRangeSlider({ minPrice, maxPrice }: IPriceRange) {
+  const setInputValue = useInputRangeSetter();
+
+  // 초기 inputValue 값을 설정
+  useEffect(() => {
+    setInputValue({
+      leftInputValue: minPrice,
+      rightInputValue: maxPrice,
+    });
+  }, []);
+
   return (
     <S.MultiRangeSlider>
       <LeftInput minPrice={minPrice} maxPrice={maxPrice} />

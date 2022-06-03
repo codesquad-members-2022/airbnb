b6bee84e6cc5958b0d6b331ce74f48193006b0a4
Kang Changgu (Miller) <rkckr1@naver.com>
2022-06-03 12:45:18 +0900

    feat: Swagger 내 Authorization 기능 추가
    - 스웨거 내에서 Header 의 Authorization 키에 들어갈 값을 입력 가능

--- backend/src/main/java/com/codesquad/airbnb/config/SwaggerConfig.java
+++ backend/src/main/java/com/codesquad/airbnb/config/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.codesquad.airbnb.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -19,7 +24,10 @@ public class SwaggerConfig {
             .apis(RequestHandlerSelectors.basePackage("com.codesquad.airbnb"))
             .paths(PathSelectors.any())
             .build()
-            .apiInfo(apiInfo());
+            .apiInfo(apiInfo())
+            .securityContexts(List.of(securityContext()))
+            .securitySchemes(List.of(apiKey()))
+            .pathMapping("/api");
     }
 
     private ApiInfo apiInfo() {
@@ -28,6 +36,22 @@ public class SwaggerConfig {
             .description("airbnb 프로젝트의 API 문서화 및 테스트를 위한 swagger")
             .version("0.0.1")
             .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+            "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
     }
 
 }

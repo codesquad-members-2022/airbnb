b0305190e5563718e4aa67684df85ed05391f380
youryu0212 <youryu0212@naver.com>
2022-06-03 10:54:25 +0900

    feat: 검색 결과창에 miniSearchBar 적용

--- fe/src/Pages/Common/SearchView.tsx
+++ fe/src/Pages/Common/SearchView.tsx
@@ -1,4 +1,5 @@
 import SearchBar from "Components/SearchBar/SearchBar";
+import { MODAL_REF_IDX } from "Helpers/constant";
 import CalendarModal from "Pages/Common/CalendarModal/CalendarModal";
 import HeadCountModal from "Pages/Common/HeadCountModal/HeadCountModal";
 import { useRef } from "react";
@@ -7,17 +8,23 @@ interface SearchViewType {
   searchBarStyle?: string;
   calendarStyle?: string;
   headCountStyle?: string;
+  searchRef?: React.MutableRefObject<HTMLElement[] | null[]>;
 }
 
-export default function SearchView({ searchBarStyle, headCountStyle, calendarStyle }: SearchViewType) {
+export default function SearchView({
+  searchRef,
+  searchBarStyle,
+  headCountStyle,
+  calendarStyle,
+}: SearchViewType) {
   const calendarRef = useRef([]);
   const headCountRef = useRef([]);
 
   return (
-    <>
+    <div ref={(el) => searchRef && (searchRef.current[MODAL_REF_IDX] = el)}>
       <SearchBar calendarRef={calendarRef} headCountRef={headCountRef} searchBarStyle={searchBarStyle} />
       <CalendarModal calendarRef={calendarRef} calendarStyle={calendarStyle} />
       <HeadCountModal headCountRef={headCountRef} headCountStyle={headCountStyle} />
-    </>
+    </div>
   );
 }
--- fe/src/Pages/SearchResult/SearchResult.styled.ts
+++ fe/src/Pages/SearchResult/SearchResult.styled.ts
@@ -1,19 +1,35 @@
+import { FlexType } from "Helpers/interface";
+import { applyFlex } from "Helpers/utils";
 import styled from "styled-components";
+
+interface SearchResultContainerType {
+  isMini?: boolean;
+}
+
+export const miniSearchBarStyle = `
+width: 700px;
+height: 76px;
+background-color: #fff;
+border-radius: 30px;
+border:1px solid #000;
+padding: 16px;
+`;
 
 export const searchBarStyle = `
 width: 916px;
 height: 76px;
 margin-top: 20px;
 margin-left: 262px;
-background-color: blue;
+background-color: #fff;
 border-radius: 30px;
+border:1px solid #000;
 padding: 16px;
 `;
 
 export const headCountStyle = `
   width:400px;
   height:355px;
-  background-color:red;
+  background-color:#fff;
   border-radius:40px;
   margin-top:16px;
   margin-left:778px;
@@ -21,7 +37,7 @@ export const headCountStyle = `
 `;
 
 export const calendarStyle = `
-background-color:red;
+background-color:#fff;
 width: 916px;
 margin-left: 262px;
 margin-top: 40px;
@@ -35,9 +51,25 @@ export const SearchResultHeader = styled.header`
   flex-direction: column;
 `;
 
-export const SearchResultContainer = styled.div`
+export const SearchResultHeaderContainer = styled.div`
+  ${({ isMini }: SearchResultContainerType) => `height:${!isMini ? "120" : "250"}px`};
+  border-bottom: 1px solid #000;
+  box-shadow: 0px 0px 20px 1px rgb(200, 200, 200);
+`;
+
+export const SearchResultHeaderArea = styled.div`
   width: 1440px;
-  height: 250px;
   margin: 0 auto;
-  border-bottom: 1px solid red;
+`;
+
+export const SearchResultArea = styled.div`
+  ${({ flex }: FlexType) => applyFlex({ flex })};
+`;
+
+export const Tourist = styled.div`
+  flex: 1;
+`;
+
+export const Map = styled.div`
+  flex: 1;
 `;

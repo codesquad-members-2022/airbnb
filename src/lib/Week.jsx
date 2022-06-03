80e2543aafa4c31f47068e545863ff7d320e2cfa
bukim0329 <bukim0329@gmail.com>
2022-06-03 15:36:30 +0900

    feat: 달력 모달 구현 중

---
+++ src/lib/Week.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {useContext} from "react";
+import styled from "styled-components";
+import {CalendarContext} from "./CalendarContext";
+
+const Week = () => {
+    const {calendarWidth} = useContext(CalendarContext);
+    const week = ["일", "월", "화", "수", "목", "금", "토"];
+    return (
+        <WeekBox calendarWidth={calendarWidth}>
+            {week.map((day, ind) => (
+                <div key={ind}>{day}</div>
+            ))}
+        </WeekBox>
+    );
+};
+
+const WeekBox = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    font-size: ${({calendarWidth}) => `${calendarWidth / 28}px`};
+    height: ${({calendarWidth}) => `${calendarWidth / 14}px`};
+    color: #828282;
+`;
+
+export default Week;

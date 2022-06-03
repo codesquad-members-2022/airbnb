89c61f4692d4aa24aa5f99fc1ff56a90b3273c02
youryu0212 <youryu0212@naver.com>
2022-06-03 10:48:31 +0900

    feat: GNB 컴포넌트 가운데 컨텐츠 props 전달받아 변경 할 수 있도록 개선

--- fe/src/Components/Gnb/Gnb.tsx
+++ fe/src/Components/Gnb/Gnb.tsx
@@ -5,7 +5,11 @@ import { Buttons, Container, Logo, Menu, Menus } from "Components/Gnb/Gnb.styled
 import { Img } from "Components/Common/styled";
 import { NavLink } from "react-router-dom";
 
-export default function Gnb() {
+interface GnBType {
+  contents?: any;
+}
+
+export default function Gnb({ contents }: GnBType) {
   return (
     <Container flex={true} justify="space-between">
       <Logo>
@@ -13,11 +17,15 @@ export default function Gnb() {
           <Img src={logo} width="88px" height="46px" />
         </NavLink>
       </Logo>
-      <Menus flex={true}>
-        <Menu>숙소</Menu>
-        <Menu>체험</Menu>
-        <Menu>온라인 체험</Menu>
-      </Menus>
+      {contents ? (
+        contents
+      ) : (
+        <Menus flex={true}>
+          <Menu>숙소</Menu>
+          <Menu>체험</Menu>
+          <Menu>온라인 체험</Menu>
+        </Menus>
+      )}
       <Buttons flex={true} justify="center" align="center">
         <Img src={drawerMenu} width="18px" height="18px" alt="메뉴" />
         <Img src={userImg} width="32px" height="32px" alt="로그인" />

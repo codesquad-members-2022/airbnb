6abb7875207c9acac2cc8b08ddf7150ae76eeb6a
WooJin Park(Ader) <ak2j38@gmail.com>
2022-06-06 23:10:08 +0900

    ✨️ 숙소검색 API 구현
    1. RoomService 숙소검색 구현
    2. 위의 구현으로 변경된 메소드 시그니쳐 ReservationService, ReservationController,
    ReservationServiceTest에 적용
    3. RoomRepositoryImpl에 QueryDsl 적
    
    end
    
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

--- BE/src/main/java/com/ahoo/airbnb/room/RoomRepositoryImpl.java
+++ BE/src/main/java/com/ahoo/airbnb/room/RoomRepositoryImpl.java
@@ -5,7 +5,7 @@ import static com.ahoo.airbnb.entity.QRoomChargePolicy.roomChargePolicy;
 
 import com.ahoo.airbnb.entity.Room;
 import com.ahoo.airbnb.reservation.chargepolicy.ChargePolicyType;
-import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -34,14 +34,26 @@ public class RoomRepositoryImpl implements RoomCustomRepository {
         return queryFactory
             .selectFrom(room)
             .where(
-                JPAExpressions
-                    .select(room.address.country
-                        .concat(room.address.city)
-                        .concat(room.address.state)
-                        .concat(room.address.street)
-                        .concat(room.address.detailAddress))
-                    .from(room).contains(address),
-                room.maxCapacity.goe(headcount))
+                containsAddress(address),
+                goeHeadcount(headcount))
             .fetch();
+    }
+
+    private BooleanExpression containsAddress(String address) {
+        if (address.isEmpty() || address.isBlank()) {
+            return null;
+        }
+        return room.address.country
+            .concat(room.address.city)
+            .concat(room.address.state)
+            .concat(room.address.street)
+            .concat(room.address.detailAddress).contains(address);
+    }
+
+    private BooleanExpression goeHeadcount(Integer headcount) {
+        if (headcount == null) {
+            return null;
+        }
+        return room.maxCapacity.goe(headcount);
     }
 }

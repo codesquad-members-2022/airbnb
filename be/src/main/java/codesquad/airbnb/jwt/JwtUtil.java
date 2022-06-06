385f508c01158fc543fad9f2ec3109e836ef2670
ikjo93 <auddlr100@naver.com>
2022-06-07 03:52:50 +0900

    refactor : 액세스 및 리프레쉬 토큰 추출 Util 메서드 생성

---
+++ be/src/main/java/codesquad/airbnb/jwt/JwtUtil.java
@@ -0,0 +1,39 @@
+package codesquad.airbnb.jwt;
+
+import java.util.NoSuchElementException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+
+public class JwtUtil {
+
+    private static final String TOKEN_TYPE = "Bearer ";
+
+    public static String getAccessToken(HttpServletRequest request) {
+        String valueOfAuthorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (valueOfAuthorizationHeader == null || !valueOfAuthorizationHeader.startsWith(TOKEN_TYPE)) {
+            throw new NoSuchElementException("access 토큰이 존재하지 않습니다.");
+        }
+
+        return valueOfAuthorizationHeader.substring(TOKEN_TYPE.length());
+    }
+
+    public static String getRefreshToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String refreshToken = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh_token")) {
+                refreshToken = cookie.getValue();
+                break;
+            }
+        }
+
+        if (refreshToken == null) {
+            throw new NoSuchElementException("refresh 토큰이 존재하지 않습니다.");
+        }
+
+        return refreshToken;
+    }
+
+}

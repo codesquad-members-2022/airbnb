46c1ce3055f0bdaf8c8db968802be32d2500c219
leekm0310 <leekm310@gmail.com>
2022-06-06 17:49:42 +0900

    Feat: 토큰에 쓰일 클래스 생성 및 members 클래스 수정
    - TokenRequest, TokenRespond, jwt, githubuser 클래스 생성
    - members 수정: 필드 변경

---
+++ src/main/java/kr/codesquad/airbnb/jwt/Jwt.java
@@ -0,0 +1,10 @@
+package kr.codesquad.airbnb.jwt;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Jwt {
+    private final String jwt;
+}

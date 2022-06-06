4c4e0b94cb3f496aa9f2bf1d354ff3dd428d3477
WooJin Park(Ader) <ak2j38@gmail.com>
2022-06-06 23:06:21 +0900

    ♻️ RoomResponse 변수명 수정 및 필드 변경
    1. charge라는 변수명이 모호해 chargePerDay로 수정
    2. wishId를 boolean isWish로 변경
    3. 위의 변경사항을 MockRoomService에 적용
    
    end
    
    Co-authored-by: who-hoo <who.ho3ov@gmail.com>

--- BE/src/main/java/com/ahoo/airbnb/room/MockRoomService.java
+++ BE/src/main/java/com/ahoo/airbnb/room/MockRoomService.java
@@ -21,15 +21,15 @@ public class MockRoomService {
             new RoomResponse(1L, "RoziHouse #건대입구역2분#소독#넷플릭스#커먼그라운드#CGV#한강", 75_000, 1_200_000, 4.7,
                 127,
                 "https://a0.muscache.com/im/pictures/f033e5ee-ae6f-48c2-9196-996444f16c4a.jpg?im_w=1200",
-                1L, "41.40338, 2.17403"),
+                true, "41.40338, 2.17403"),
             new RoomResponse(2L, "도시속영화관 #2 빔프로젝트, 왕십리역1분, 한양대병원 .시티뷰, 넷플릭스", 60_000, 1_000_539,
                 4.5, 95,
                 "https://a0.muscache.com/im/pictures/miso/Hosting-47988590/original/12c9f003-22bf-4df0-8ea0-1ca8a5569f6c.jpeg?im_w=1200",
-                2L, "41.40990, 2.15235"),
+                true, "41.40990, 2.15235"),
             new RoomResponse(3L, "그레이하우스 서울대입구 5분거리 샤로수길 맞은편 #강남 #홍대 #2호선 #GrayHouse", 60_000,
                 1_000_539, 4.5, 95,
                 "https://a0.muscache.com/im/pictures/f71636a9-1091-40c3-bea7-0d1ae6794428.jpg?im_w=1200",
-                null, "25.40990, 4.15235")
+                false, "25.40990, 4.15235")
 
         );
     }

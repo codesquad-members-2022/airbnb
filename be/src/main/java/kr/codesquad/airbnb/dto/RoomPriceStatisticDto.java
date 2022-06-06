65e57fd27539cb4608830e6455c773b9a89ac179
dongkyu <dongkyu.parkk@gmail.com>
2022-06-07 01:51:52 +0900

    refactor: Room List -> 통계Dto 변환 메서드 작게 분리 (CODESQUAD-AIRBNB/airbnb#26)

--- be/src/main/java/kr/codesquad/airbnb/dto/RoomPriceStatisticDto.java
+++ be/src/main/java/kr/codesquad/airbnb/dto/RoomPriceStatisticDto.java
@@ -25,20 +25,31 @@ public class RoomPriceStatisticDto {
             totalPricePerNightRooms += pricePerNight;
 
             int priceTag = pricePerNight / CategorizedPrice.RANGE_OF_PRICE * CategorizedPrice.RANGE_OF_PRICE;
-
-            if (categorizedPriceMap.containsKey(priceTag)) {
-                categorizedPriceMap.get(priceTag).addRoomCount();
-                continue;
-            }
-
-            categorizedPriceMap.put(priceTag, new CategorizedPrice(priceTag));
+            updateCategorizedPriceMap(categorizedPriceMap, priceTag);
         }
 
-        avgPricePerNight = totalPricePerNightRooms / rooms.size();
-        countOfCategorizedPricePerNight = new ArrayList<>(categorizedPriceMap.values());
-        Collections.sort(countOfCategorizedPricePerNight);
+        setAvgPricePerNight(totalPricePerNightRooms, rooms.size());
+        setCountOfCategorizedPricePerNight(categorizedPriceMap);
 
         return this;
+    }
+
+    private void setCountOfCategorizedPricePerNight(Map<Integer, CategorizedPrice> categorizedPriceMap) {
+        countOfCategorizedPricePerNight = new ArrayList<>(categorizedPriceMap.values());
+        Collections.sort(countOfCategorizedPricePerNight);
+    }
+
+    private void setAvgPricePerNight(int totalPricePerNightRooms, int roomCount) {
+        avgPricePerNight = totalPricePerNightRooms / roomCount;
+    }
+
+    private void updateCategorizedPriceMap(Map<Integer, CategorizedPrice> categorizedPriceMap, int priceTag) {
+        if (categorizedPriceMap.containsKey(priceTag)) {
+            categorizedPriceMap.get(priceTag).addRoomCount();
+            return;
+        }
+
+        categorizedPriceMap.put(priceTag, new CategorizedPrice(priceTag));
     }
 
     private void compareAndUpdateMinPrice(Integer pricePerNight) {

e68ef7c6117e41c1eadd0d79526a70368c6e2e83
godhyeongman <aaaa4000@naver.com>
2022-06-06 23:00:24 +0900

    refactor: 날짜 관련 데이터 처리 비즈니스 로직 분리

--- airbnb/src/libs/Schedule/Components/Day.tsx
+++ airbnb/src/libs/Schedule/Components/Day.tsx
@@ -1,29 +1,15 @@
 import { useScheduleContext } from '../Hooks/ScheduleHooks';
 import * as S from './Schedule.style';
+import {
+  isBetweenSelected,
+  isStartDate,
+  isEndDate,
+  isSelected,
+} from '@/Services/Date';
 
 interface DayProps {
   day: Date | null;
 }
-
-const isStartDate = (day: Date | null, startDate: Date | null): boolean =>
-  !!day && startDate?.getTime() === day.getTime();
-
-const isEndDate = (day: Date | null, endDate: Date | null) =>
-  !!day && endDate?.getTime() === day.getTime();
-
-function isSelected(
-  day: Date | null,
-  startDate: Date | null,
-  endDate: Date | null,
-) {
-  return (!!day && isStartDate(day, startDate)) || isEndDate(day, endDate);
-}
-
-const isBetweenSelected = (
-  day: Date | null,
-  startDate: Date | null,
-  endDate: Date | null,
-) => !!day && !!startDate && !!endDate && startDate <= day && endDate >= day;
 
 export function Day({ day }: DayProps): JSX.Element {
   // TODO: 최적화 리렌더링 막아야함

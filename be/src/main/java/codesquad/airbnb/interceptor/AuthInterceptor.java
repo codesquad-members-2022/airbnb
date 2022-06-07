883259e278db484c0361de06a1201fcf4edf315d
ikjo93 <auddlr100@naver.com>
2022-06-07 16:31:02 +0900

    fix : 숙소 예약 API jwt 검증 로직 수정
    * 당초 숙소 예약 API jwt 검증 로직에서 access token을 재발급해주는 기능도 수행하고 있었지만 이는 별도 API로 구성하는 것이 적합하다고 생가

--- be/src/main/java/codesquad/airbnb/interceptor/AuthInterceptor.java
+++ be/src/main/java/codesquad/airbnb/interceptor/AuthInterceptor.java
@@ -1,14 +1,10 @@
 package codesquad.airbnb.interceptor;
 
-import codesquad.airbnb.dto.LoginResponse;
-import codesquad.airbnb.jwt.JwtManager;
-import codesquad.airbnb.jwt.JwtProvider;
+import codesquad.airbnb.dto.ResponseMessage;
 import codesquad.airbnb.jwt.JwtUtil;
 import codesquad.airbnb.jwt.JwtValidator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.NoSuchElementException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +16,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @RequiredArgsConstructor
 public class AuthInterceptor implements HandlerInterceptor {
 
-    private static final String TOKEN_TYPE = "Bearer ";
-
     private final JwtValidator jwtValidator;
-    private final JwtManager jwtManager;
-    private final JwtProvider jwtProvider;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+        throws IOException {
         if (!validateThatHttpMethodIsPost(request)) {
             return true;
         }
@@ -37,14 +30,7 @@ public class AuthInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        String refreshToken = JwtUtil.getRefreshToken(request);
-        String email = jwtValidator.getPayload(refreshToken);
-
-        if (!jwtValidator.validateExpirationOfToken(refreshToken)) {
-            throw new NoSuchElementException("refresh 토큰의 만료기한이 지났습니다. 다시 로그인 해주세요.");
-        }
-
-        issueRenewedAccessToken(response, email);
+        setResponseBody(response);
 
         return false;
     }
@@ -54,34 +40,9 @@ public class AuthInterceptor implements HandlerInterceptor {
         return httpMethod.equals("POST");
     }
 
-    private void issueRenewedAccessToken(HttpServletResponse response, String email) throws IOException {
-        String refreshToken = jwtManager.getRefreshTokenByEmail(email);
-
-        if (refreshToken == null) {
-            throw new NoSuchElementException("refresh 토큰이 존재하지 않습니다.");
-        }
-
-        String renewedAccessToken = jwtProvider.createAccessToken(email);
-        LoginResponse loginResponse = LoginResponse.builder()
-            .email(email)
-            .tokenType(TOKEN_TYPE)
-            .accessToken(renewedAccessToken)
-            .refreshToken(refreshToken)
-            .build();
-
-        setResponseHeader(response, renewedAccessToken);
-        setResponseBody(response, loginResponse);
-    }
-
-    private void setResponseHeader(HttpServletResponse response, String renewedAccessToken) {
-        response.setStatus(HttpStatus.OK.value());
-        response.setContentType("application/json");
-        response.setCharacterEncoding(String.valueOf(StandardCharsets.UTF_8));
-        response.addHeader("access_token", renewedAccessToken);
-    }
-
-    private void setResponseBody(HttpServletResponse response, LoginResponse responseMessage) throws IOException {
+    private void setResponseBody(HttpServletResponse response) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
+        ResponseMessage responseMessage = new ResponseMessage(HttpStatus.FORBIDDEN, "access 토큰이 만료되었습니다.");
         String stringOfResponseMessage = mapper.writeValueAsString(responseMessage);
         response.getWriter().write(stringOfResponseMessage);
     }

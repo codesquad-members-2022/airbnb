b2b01ba05af06984e5e31502ea89abe027211250
asqw887 <asqw887@gmail.com>
2022-06-07 15:23:11 +0900

    Feat: PriceSettingVC&View 구현

--- iOS/Targets/Cherrybnb/Sources/Search_Price/View/PriceSettingView.swift
+++ iOS/Targets/Cherrybnb/Sources/Search_Price/View/PriceSettingView.swift
@@ -9,13 +9,76 @@
 import UIKit
 
 class PriceSettingView: UIView {
+    
+    var price: Price?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "가격 범위"
+        label.font = UIFont.cherrybnbBoldBody
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var rangeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.cherrybnbBody
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var averageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .systemGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setSubviews()
+        setLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    private func setSubviews(){
+        self.addSubview(titleLabel)
+        self.addSubview(rangeLabel)
+        self.addSubview(averageLabel)
+    }
+    
+    private func setLayout(){
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 32),
+            titleLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            titleLabel.heightAnchor.constraint(equalToConstant: 22)
+        ])
+        
+        NSLayoutConstraint.activate([
+            rangeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            rangeLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            rangeLabel.heightAnchor.constraint(equalToConstant: 22)
+        ])
+        
+        NSLayoutConstraint.activate([
+            averageLabel.topAnchor.constraint(equalTo: rangeLabel.bottomAnchor, constant: 8),
+            averageLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            averageLabel.heightAnchor.constraint(equalToConstant: 16)
+        ])
+
+    }
+    
+    func setComponents(_ price: Price) {
+        rangeLabel.text = "\(price.min) - \(price.max)"
+        averageLabel.text = "평균 1박 요금 가격은 \(price.average)원 입니다."
+    }
 
 }
--- iOS/Targets/Cherrybnb/Sources/Search_Price/ViewController/PriceSettingViewController.swift
+++ iOS/Targets/Cherrybnb/Sources/Search_Price/ViewController/PriceSettingViewController.swift
@@ -9,22 +9,53 @@
 import UIKit
 
 class PriceSettingViewController: UIViewController {
+    
+    private var priceSettingView = PriceSettingView()
+    var queryParameter: QueryParameter?
+    
+    // TODO: Merge 후 QueryParameterStackView 로 변경
+    lazy var queryParameterView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .white
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let priceSuccessStubRequest = SearchManager(httpService: PriceSuccessStub())
+        priceSuccessStubRequest.searchPriceFrequency(queryComponent: queryParameter ?? QueryParameter()) { [weak self] price in
+            guard let price = price, let self = self else { return }
+            self.priceSettingView.setComponents(price)
+        }
+        setSubviews()
+        setLayout()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setSubviews() {
+        view.addSubview(priceSettingView)
+        view.addSubview(queryParameterView)
     }
-    */
+
+    private func setLayout() {
+        priceSettingView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            priceSettingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            priceSettingView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            priceSettingView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            priceSettingView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.7)
+        ])
+
+        NSLayoutConstraint.activate([
+            queryParameterView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            queryParameterView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            queryParameterView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            queryParameterView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.25)
+        ])
+    }
 
 }

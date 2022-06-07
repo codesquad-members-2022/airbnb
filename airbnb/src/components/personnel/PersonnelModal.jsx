f5bc398de34efc70d9e2dd094233bb8835536995
seungchanHan <seungchanhan00@gmail.com>
2022-06-08 01:38:13 +0900

    feat : [19] 게스트 정보 변경

--- airbnb/src/components/personnel/PersonnelModal.jsx
+++ airbnb/src/components/personnel/PersonnelModal.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import {
+  PersonnelContext,
+  PersonnelSetterContext,
+} from 'contexts/PersonnelProvider';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import PersonnelItem from './PersonnelItem';
 
 function PersonnelModal() {
+  const { adultsNum, childrenNum, babiesNum } = useContext(PersonnelContext);
+  const { setAdultsNum, setChildrenNum, setBabiesNum } = useContext(
+    PersonnelSetterContext,
+  );
+
   const guestInfos = [
-    { title: '성인', desc: '만 13세 이상', state: 0 },
-    { title: '어린이', desc: '만 2~12세', state: 0 },
-    { title: '유아', desc: '만 2세 미만', state: 0 },
+    {
+      title: '성인',
+      desc: '만 13세 이상',
+      state: adultsNum,
+      setState: setAdultsNum,
+    },
+    {
+      title: '어린이',
+      desc: '만 2~12세',
+      state: childrenNum,
+      setState: setChildrenNum,
+    },
+    {
+      title: '유아',
+      desc: '만 2세 미만',
+      state: babiesNum,
+      setState: setBabiesNum,
+    },
   ];
 
   return (

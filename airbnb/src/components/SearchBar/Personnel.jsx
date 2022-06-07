9f6d5e08f786a36366045ad96204702894d2bcf1
seungchanHan <seungchanhan00@gmail.com>
2022-06-08 01:38:52 +0900

    feat : [19] 인원 정보 렌더

--- airbnb/src/components/SearchBar/Personnel.jsx
+++ airbnb/src/components/SearchBar/Personnel.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { PersonnelContext } from 'contexts/PersonnelProvider.jsx';
+import React, { useContext } from 'react';
 import { Btn, ContentBox, BarTitle, BarContent } from './SearchBar_styled.jsx';
 
 function Personnel({ onClick }) {
+  const { adultsNum, childrenNum, babiesNum } = useContext(PersonnelContext);
+
+  let guestContent = '';
+  if (adultsNum > 0) guestContent += `성인 ${adultsNum}명`;
+  if (childrenNum > 0) guestContent += `, 어린이 ${childrenNum}명`;
+  if (babiesNum > 0) guestContent += `, 유아 ${babiesNum}명`;
+  if (adultsNum === 0) guestContent = '게스트 추가';
+
   return (
     <Btn>
       <ContentBox onClick={() => onClick('TOTAL_GUESTS')}>
         <BarTitle>인원</BarTitle>
-        <BarContent>게스트 추가</BarContent>
+        <BarContent>{guestContent}</BarContent>
       </ContentBox>
     </Btn>
   );

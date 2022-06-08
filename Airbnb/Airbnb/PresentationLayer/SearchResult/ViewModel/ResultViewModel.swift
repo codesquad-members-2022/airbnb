85d2fc17e1177ebe7bd3f84c648c807dcef08b50
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-06-08 17:24:19 +0900

    chore: resolve conflict

--- Airbnb/Airbnb/PresentationLayer/SearchResult/ViewModel/ResultViewModel.swift
+++ Airbnb/Airbnb/PresentationLayer/SearchResult/ViewModel/ResultViewModel.swift
@@ -10,9 +10,21 @@ final class ResultViewModel {
 
     var filterSelection: Observable<FilterSelection>
     private let searchAccomadationUseCase: SearchAccomadationUseCase = DefaultSearchAccomadationUseCase()
+    var testNetwork  = NetworkManager()
 
     init(selectionResult: FilterSelection) {
         filterSelection = Observable<FilterSelection>(selectionResult)
     }
-
+    
+    func load(){
+        testNetwork.request(path: "/rooms?", completion: { result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let data):
+                print(data)
+                return
+            }
+        })
+    }
 }

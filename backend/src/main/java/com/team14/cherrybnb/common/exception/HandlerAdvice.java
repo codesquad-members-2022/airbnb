409511b9286e854d53d6e001fcef546a90d0f287
naneun <rmtlrksdp@naver.com>
2022-06-08 17:11:36 +0900

    [#46] refactor: JsonProcessing, ParseException 처리 추가

--- backend/src/main/java/com/team14/cherrybnb/common/exception/HandlerAdvice.java
+++ backend/src/main/java/com/team14/cherrybnb/common/exception/HandlerAdvice.java
@@ -1,8 +1,13 @@
 package com.team14.cherrybnb.common.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.locationtech.jts.io.ParseException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
 
 @RestControllerAdvice
 public class HandlerAdvice {
@@ -10,5 +15,15 @@ public class HandlerAdvice {
     @ExceptionHandler(BusinessException.class)
     private ResponseEntity<ErrorResponse<String>> handleException(BusinessException e) {
         return new ResponseEntity<>(new ErrorResponse<>(e.getMessage()), e.getStatus());
+    }
+
+    @ExceptionHandler(IOException.class)
+    private ResponseEntity<ErrorResponse<String>> handleException(IOException e) {
+        return new ResponseEntity<>(new ErrorResponse<>(e.getMessage()), HttpStatus.BAD_GATEWAY);
+    }
+
+    @ExceptionHandler(ParseException.class)
+    private ResponseEntity<ErrorResponse<String>> handleException(ParseException e) {
+        return new ResponseEntity<>(new ErrorResponse<>(e.getMessage()), HttpStatus.BAD_GATEWAY);
     }
 }

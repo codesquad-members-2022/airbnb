66a55428ac17e31939100ac2111996cd6817fd1a
Kang Changgu (Miller) <rkckr1@naver.com>
2022-06-09 18:06:27 +0900

    docs: GithubAuthController 에 태그 추가

--- backend/src/main/java/com/codesquad/airbnb/auth/controller/GithubAuthController.java
+++ backend/src/main/java/com/codesquad/airbnb/auth/controller/GithubAuthController.java
@@ -7,6 +7,8 @@ import com.codesquad.airbnb.auth.domain.GithubToken;
 import com.codesquad.airbnb.auth.domain.GithubUser;
 import com.codesquad.airbnb.auth.domain.OAuthProperties;
 import com.codesquad.airbnb.core.member.Member;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "Github OAuth API")
 @RestController
 @RequestMapping("/auth/github")
 public class GithubAuthController extends AbstractAuthController {

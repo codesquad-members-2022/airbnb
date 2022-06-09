6b9db48dc9758b8a3f3a5fff365490358639dd66
dongkyu <dongkyu.parkk@gmail.com>
2022-06-09 12:58:02 +0900

    fix: 체크아웃 = 체크인 날짜인 경우에도, 불가능함을 알리는 값 검증 메시지 추가

--- be/src/main/java/kr/codesquad/airbnb/exception/ErrorCode.java
+++ be/src/main/java/kr/codesquad/airbnb/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
 
 	INVALID_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "요청에 필요한 필수 파라미터 값이 존재하지 않습니다."),
 //	FORBIDDEN_CHECK_IN_DATE_PAST(HttpStatus.BAD_REQUEST, "체크인 날짜는 현재 시간보다 이전 일 수 없습니다."),	checkIn 날짜 과거 설정 검증 개발 편의를 위해 주석처리
-	FORBIDDEN_CHECK_OUT_DATE_IS_BEFORE_CHECK_IN(HttpStatus.BAD_REQUEST, "체크아웃 날짜는 체크인 날짜보다 이전 일 수 없습니다."),
+	FORBIDDEN_CHECK_OUT_DATE_IS_BEFORE_CHECK_IN(HttpStatus.BAD_REQUEST, "체크아웃 날짜는 체크인 날짜와 같거나 이전 일 수 없습니다."),
 	FORBIDDEN_USER(HttpStatus.FORBIDDEN, "요청 권한이 없는 사용자 입니다."),
 	NOT_EXIST_ROOM(HttpStatus.NOT_FOUND, "존재하지 않는 숙소 입니다."),
 	NOT_EXIST_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자 입니다."),

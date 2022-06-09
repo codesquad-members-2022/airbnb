98dd8088642985b48cd6161ab162c92ac60a73d8
youryu0212 <youryu0212@naver.com>
2022-06-09 19:01:23 +0900

    design: 검색창, 요금 모달 css 스타일 변경

--- fe/src/Pages/Common/PriceModal/PriceModal.styled.ts
+++ fe/src/Pages/Common/PriceModal/PriceModal.styled.ts
@@ -3,10 +3,11 @@ import { applyFlex } from "Helpers/utils";
 import styled from "styled-components";
 
 export const PriceModalContainer = styled.div<FlexType>`
-  position: relative;
+  position: absolute;
   width: 500px;
   height: 364px;
   background-color: #fff;
+  box-shadow: 0px 4px 10px rgba(51, 51, 51, 0.1), 0px 0px 4px rgba(51, 51, 51, 0.05);
   margin-top: 40px;
   margin-left: 600px;
   border-radius: 40px;

8d6389037774b19097f39109cfcb1b2db37c0e7a
suuzin <8oozin@gmail.com>
2022-06-10 09:17:56 +0900

    refactor : if문 변경 (CODESQUAD-AIRBNB/airbnb#23)

--- FE/src/components/MainContent/MainSection.tsx
+++ FE/src/components/MainContent/MainSection.tsx
@@ -8,23 +8,24 @@ interface MainSectionType {
 
 const MainSection = ({ data }: MainSectionType) => {
   const getContentSection = () => {
-    if (data.type === 'city') {
-      return data.content.map(({ id, image, name, description }) => (
-        <S.SectionItem key={id} size={IMG_SIZE.SMALL}>
-          <S.ImgWrapper src={image} />
-          <div>
-            <S.Name>{name}</S.Name>
-            <S.Description>{description}</S.Description>
-          </div>
-        </S.SectionItem>
-      ));
-    } else {
-      return data.content.map(({ id, image, name }) => (
-        <S.SectionItem key={id} size={IMG_SIZE.LARGE}>
-          <S.ImgWrapper src={image} />
-          <S.Description>{name}</S.Description>
-        </S.SectionItem>
-      ));
+    switch (data.type) {
+      case 'city':
+        return data.content.map(({ id, image, name, description }) => (
+          <S.SectionItem key={id} size={IMG_SIZE.SMALL}>
+            <S.ImgWrapper src={image} />
+            <div>
+              <S.Name>{name}</S.Name>
+              <S.Description>{description}</S.Description>
+            </div>
+          </S.SectionItem>
+        ));
+      case 'trip':
+        return data.content.map(({ id, image, name }) => (
+          <S.SectionItem key={id} size={IMG_SIZE.LARGE}>
+            <S.ImgWrapper src={image} />
+            <S.Description>{name}</S.Description>
+          </S.SectionItem>
+        ));
     }
   };
   return (

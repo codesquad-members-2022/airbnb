0d56b6c27ca8e532517cff26660e24676e7c60f3
suuzin <8oozin@gmail.com>
2022-06-10 09:22:08 +0900

    refactor : 변수명변경 (CODESQUAD-AIRBNB/airbnb#23)

--- FE/src/components/Calendar/Day.tsx
+++ FE/src/components/Calendar/Day.tsx
@@ -1,59 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { CalendarTypes } from '@components/Calendar';
-import { useCalendarState } from '@lib/hooks/useContext';
+import Test from '@components/Calendar/Test';
 
 const Day = ({ year, month }: CalendarTypes) => {
   const date = new Date();
-  const lastDay = new Date(year, month + 1, 0).getDate();
-  const firstDayIndex = new Date(year, month, 1).getDay();
+  const daysInCurMonth = new Date(year, month + 1, 0).getDate();
+  const daysInPrevMonth = new Date(year, month, 1).getDay();
   const lastDayIndex = new Date(date.getFullYear(), month + 1, 0).getDay();
-  const nextDays = 7 - lastDayIndex - 1;
-
-  const [isCheckIn, setIsCheckIn] = useState(false);
-
-  const { setCheckIn, setCheckOut } = useCalendarState();
-
-  const [isSelected, setIsSelected] = useState(false);
-
-  const onClick = ({ target }: any) => {
-    if (!isCheckIn) {
-      setIsSelected(!isSelected);
-
-      setCheckIn(`${month + 1}월 ${target.innerText}일`);
-      setIsCheckIn(!isCheckIn);
-    } else {
-      setCheckOut(`${month + 1}월 ${target.innerText}일`);
-    }
-  };
+  const daysInNextMonth = 7 - lastDayIndex - 1;
 
   const days = [];
-
-  for (let prev = firstDayIndex; prev > 0; prev--) {
-    days.push('');
+  for (let prevDay = daysInPrevMonth; prevDay > 0; prevDay--) {
+    days.push(0);
   }
-
-  for (let i = 1; i <= lastDay; i++) {
+  for (let i = 1; i <= daysInCurMonth; i++) {
     days.push(i);
   }
-
-  for (let j = 1; j <= nextDays; j++) {
-    days.push('');
+  for (let nextDay = 1; nextDay <= daysInNextMonth; nextDay++) {
+    days.push(0);
   }
-
   return (
     <>
-      {days.map((day, index) =>
-        day != '' ? (
-          <button key={index} onClick={onClick}>
-            {day}
-          </button>
-        ) : (
-          <button key={index} disabled>
-            {}
-          </button>
-        ),
-      )}
+      {days.map((day, index) => (
+        <Test year={year} month={month} day={day} index={index} />
+      ))}
     </>
   );
 };

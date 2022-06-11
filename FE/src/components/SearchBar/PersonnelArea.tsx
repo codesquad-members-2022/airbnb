546b56dddf5104703e246a63e33e3f6f58679a8f
mina-gwak <mina.gwak@kakao.com>
2022-06-11 23:03:42 +0900

    feat: 인원 모달 생성 후 렌더링 (CODESQUAD-AIRBNB/airbnb#31)

--- FE/src/components/SearchBar/PersonnelArea.tsx
+++ FE/src/components/SearchBar/PersonnelArea.tsx
@@ -1,45 +1,61 @@
+import { useRef } from 'react';
+
+import PersonnelModal from '@components/SearchBar/Modal/PersonnelModal';
 import { AreaPropsTypes } from '@components/SearchBar/PriceArea';
 import * as S from '@components/SearchBar/SearchBar.style';
 import { AREA_TYPE, NO_CONTENT, SEARCH_BAR_SIZE } from '@components/SearchBar/constants';
 import Icon, { ICON_NAME, ICON_SIZE } from '@components/common/Icon';
-import { PersonnelTypes, defaultPersonnel } from '@data';
-
-const getPersonnelContent = (personnel: PersonnelTypes) => {
-  return Object.entries(personnel)
-    .reduce((content: string[], [key, value]) => {
-      value && content.push(`${key} ${value}명`);
-      return content;
-    }, [])
-    .join(', ');
-};
+import { PersonnelContextTypes, PersonnelProvider } from '@context/personnel/Provider';
+import WithProvider from '@hoc/WithProvider';
+import { usePersonnelState } from '@lib/hooks/useContext';
+import useModal from '@lib/hooks/useModal';
 
 const PersonnelArea = ({ size }: AreaPropsTypes) => {
-  const { guest, kid } = defaultPersonnel;
-  const isPersonnelExist = guest !== defaultPersonnel.guest || kid !== defaultPersonnel.kid;
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [isPersonnelModalOpen, setIsPersonnelModalOpen] = useModal({ modalRef });
+  const { numberOfAdults, numberOfChildren, numberOfBabies, resetAllCount } =
+    usePersonnelState() as PersonnelContextTypes;
+
+  const isPersonnelExist = numberOfAdults + numberOfChildren + numberOfBabies !== 0;
+
+  const getPersonnelContent = () => {
+    let content = [];
+    if (numberOfAdults + numberOfChildren) content.push(`게스트 ${numberOfAdults + numberOfChildren}명`);
+    if (numberOfBabies) content.push(`유아 ${numberOfBabies}명`);
+    return content.join(', ');
+  };
 
   const personnelContent = isPersonnelExist
-    ? getPersonnelContent(defaultPersonnel)
+    ? getPersonnelContent()
     : NO_CONTENT[AREA_TYPE.PERSONNEL];
 
+  const openPersonnelModal = () => setIsPersonnelModalOpen(true);
+
   return (
-    <S.PersonnelArea>
-      {size === SEARCH_BAR_SIZE.LARGE ? (
-        <>
-          <S.ContentContainer>
-            <S.Label>인원</S.Label>
-            <S.Content isContentExist={isPersonnelExist}>{personnelContent}</S.Content>
-          </S.ContentContainer>
-          {isPersonnelExist && (
-            <S.CloseButton>
-              <Icon iconName={ICON_NAME.CLOSE_BTN} iconSize={ICON_SIZE.LARGE} />
-            </S.CloseButton>
-          )}
-        </>
-      ) : (
-        <S.Content isContentExist={isPersonnelExist}>{personnelContent}</S.Content>
-      )}
-    </S.PersonnelArea>
+    <>
+      <S.PersonnelArea onClick={openPersonnelModal}>
+        {size === SEARCH_BAR_SIZE.LARGE ? (
+          <>
+            <S.ContentContainer>
+              <S.Label>인원</S.Label>
+              <S.Content isContentExist={isPersonnelExist}>{personnelContent}</S.Content>
+            </S.ContentContainer>
+            {isPersonnelExist && (
+              <S.CloseButton onClick={resetAllCount}>
+                <Icon iconName={ICON_NAME.CLOSE_BTN} iconSize={ICON_SIZE.LARGE} />
+              </S.CloseButton>
+            )}
+          </>
+        ) : (
+          <S.Content isContentExist={isPersonnelExist}>{personnelContent}</S.Content>
+        )}
+      </S.PersonnelArea>
+      {isPersonnelModalOpen && <PersonnelModal modalRef={modalRef} />}
+    </>
   );
 };
 
-export default PersonnelArea;
+export default WithProvider({
+  Component: PersonnelArea,
+  Provider: PersonnelProvider,
+});
